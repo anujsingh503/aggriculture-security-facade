00aca1bb4f9e6ef785da96989fa6d7eaaac6222d
Anuj Singh <anujs503@gmail.com>
2021-08-19 00:20:50 +0100

    Project initialized

---
+++ src/main/java/com/auth/AggricultureSecurityFacadeApplication.java
@@ -0,0 +1,13 @@
+package com.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggricultureSecurityFacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggricultureSecurityFacadeApplication.class, args);
+	}
+
+}
